4d58ca401270874d3df2d785c5fbc82e007b9521
dfdvdsf <81430928+song682@users.noreply.github.com>
2025-08-31 23:26:32 +0800

    Ahhhhhhhh

--- src/main/java/decok/dfcdvadstf/createworldui/api/util/TextureManager.java
+++ src/main/java/decok/dfcdvadstf/createworldui/api/util/TextureManager.java
@@ -6,7 +6,6 @@ public class TextureManager {
     public static final ResourceLocation TABS = new ResourceLocation("createworldui", "textures/gui/tabs.png");
     public static final ResourceLocation OPTIONS_BG_DARK = new ResourceLocation("createworldui", "textures/gui/options_bg_dark.png");
     public static final ResourceLocation OPTIONS_BG_LIGHT = new ResourceLocation("createworldui", "textures/gui/options_bg_light.png");
-    public static final ResourceLocation BUTTONS = new ResourceLocation("createworldui", "textures/gui/buttons.png");
 
     // 选项卡纹理坐标
     public static final int TAB_NORMAL_U = 0;
@@ -20,4 +19,5 @@ public class TextureManager {
 
     public static final int TAB_WIDTH = 130;
     public static final int TAB_HEIGHT = 24;
+
 }
