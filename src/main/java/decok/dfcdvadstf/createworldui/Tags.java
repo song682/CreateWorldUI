ce7a1273700f4e59d99cbd7e6bb63795dfe31ece
dfdvdsf <81430928+song682@users.noreply.github.com>
2025-09-01 23:00:24 +0800

    Delete src/main/java/decok/dfcdvadstf/createworldui directory

--- src/main/java/decok/dfcdvadstf/createworldui/Tags.java
+++
@@ -1,7 +0,0 @@
-package decok.dfcdvadstf.createworldui;
-
-public class Tags {
-    public static final String MODID = "createworldui";
-    public static final String NAME = "CreateWorldUI";
-    public static final String VERSION = "0.0.1";
-}
