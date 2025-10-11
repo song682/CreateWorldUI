1236700285f10de115afb3179d5a713b68714496
dfdvdsf <81430928+song682@users.noreply.github.com>
2025-10-11 21:28:46 +0800

    {TabState}: Keep all of the text color become white. (#13)

--- src/main/java/decok/dfcdvadstf/createworldui/api/TabState.java
+++ src/main/java/decok/dfcdvadstf/createworldui/api/TabState.java
@@ -1,10 +1,10 @@
 package decok.dfcdvadstf.createworldui.api;
 
 public enum TabState {
-    NORMAL(0, 0, 0xE0E0E0),
+    NORMAL(0, 0, 0xFFFFFF),
     HOVER(0, 24, 0xFFFFFF),
-    SELECTED(0, 48, 0xFFCC00),
-    SELECTED_HOVER(0, 72, 0xFFCC00);
+    SELECTED(0, 48, 0xFFFFFF),
+    SELECTED_HOVER(0, 72, 0xFFFFFF);
 
     public final int u;
     public final int v;
