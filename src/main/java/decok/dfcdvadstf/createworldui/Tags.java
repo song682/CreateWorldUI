38936df1293348de9f28e4a4dc3c74f4419d5c47
dfdvdsf <81430928+song682@users.noreply.github.com>
2025-10-25 10:31:50 +0800

    Update Tags.java

--- src/main/java/decok/dfcdvadstf/createworldui/Tags.java
+++ src/main/java/decok/dfcdvadstf/createworldui/Tags.java
@@ -3,6 +3,7 @@ package decok.dfcdvadstf.createworldui;
 public class Tags {
     public static final String MODID = "createworldui";
     public static final String NAME = "CreateWorldUI";
-    public static final String VERSION = "0.4.5";
+    public static final String VERSION = "0.5.0";
 }
+
 
